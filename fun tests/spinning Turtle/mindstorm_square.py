0358f2088ed9c87ca96778097d17cefebf4b02cc
Dokara_S <majid_2010_10@hotmail.com>
2019-04-26 19:58:16 +0300

    Add files via upload

---
+++ fun tests/spinning Turtle/mindstorm_square.py
@@ -0,0 +1,21 @@
+import turtle
+
+def draw_square(some_turtle):
+        for i  in range(1,5):
+                some_turtle.forward(200)
+                some_turtle.right(90)
+
+def draw_art():
+        window = turtle.Screen()
+        window.bgcolor("black")
+
+        brad = turtle.Turtle()
+        brad.shape("turtle")
+        brad.color("yellow")
+        brad.speed(500)
+        for i in range(1,74):
+                draw_square(brad)
+                brad.right(5)
+        
+        window.exitonclick()
+draw_art()
